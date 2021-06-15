55a6ebf4371d591a5292a522d7e343e845a6a01d
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-14 20:59:56 -0400

    Add files via upload

---
+++ objectIdentification/main.js
@@ -0,0 +1,41 @@
+img="";
+objects = [];
+status="";
+
+function preload(){
+    img= loadImage('dogCat.png');
+    }
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+   
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+
+    }
+    console.log(results);
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Cat", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect( 30, 60, 450, 350 );
+    fill("#FFOOOO");
+    text("Dog", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+    }
